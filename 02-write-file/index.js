c7bef95509a4ddf2945ed2d5f3ddde3aacf6b248
Olgasinkevich <sinkevichweb@gmail.com>
2021-11-09 01:35:49 +0300

    feat: add 02-write-file, 3-files-in-folder

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname, '02-write-file', 'text.txt');
-fs.writeFile(filePath, 'Hello', err => {
-  if(err) {
+
+
+const filePath = path.join('02-write-file', 'text.txt');
+
+fs.writeFile(filePath, '', err => {
+  if (err) {
     throw err;
   }
   console.log('Enter text');
 });
 
-fs.appendFile(filePath, 'Entered text', err => {
+fs.appendFile(filePath,'', err => {
   if (err) {
     throw err;
   }
-  console.log ('Enter text');
 });
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,2 +1,4 @@
-//const path = require('path')
-//console.log(path.dirname(__filename),"-" path.extname(__filename))
+const path = require('path');
+
+const filePath = path.join(__dirname, 'secret-folder');
+console.log(`'${path.dirname(__filename)}' - '${path.extname(__filename)}'`);
